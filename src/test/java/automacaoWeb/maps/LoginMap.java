6e285a27e8dc3f7dcf922a2fe2dffe87de0c5db9
elen <elen.crozzara@gmail.com>
2023-05-19 07:04:18 -0300

    Criacao das classes LoginMap para mapear os elementos e LoginPage que vai chamar os elementos inseridos na classe LoginMap

---
+++ src/test/java/automacaoWeb/maps/LoginMap.java
@@ -0,0 +1,15 @@
+package automacaoWeb.maps;
+
+import org.openqa.selenium.WebElement;
+
+public class LoginMap {
+    public WebElement btnLogin;
+    public WebElement btnFechar;
+    public WebElement divFecharModal;
+    public WebElement inpUserName;
+    public WebElement inpPassword;
+    public WebElement btnSignIn;
+    public WebElement inpRemember;
+    public WebElement linkCreateAccount;
+
+}
