6eb3e6e54f55708910f2d3206bf334f1197f709f
elen <elen.crozzara@gmail.com>
2023-05-19 08:57:35 -0300

    Criacao dos metodos de cada elemento da classe LoginPage

--- src/test/java/automacaoWeb/pages/LoginPage.java
+++ src/test/java/automacaoWeb/pages/LoginPage.java
@@ -1,4 +1,40 @@
 package automacaoWeb.pages;
 
+import automacaoWeb.core.Driver;
+import automacaoWeb.maps.LoginMap;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+    LoginMap loginMap;
+    public LoginPage(){
+        loginMap = new LoginMap();
+        PageFactory.initElements(Driver.getDriver(), loginMap);
+    }
+    public void clickBtnLogin(){
+        loginMap.btnLogin.click();
+    }
+    public void clickBtnFechar(){
+        loginMap.btnFechar.click();
+    }
+    public void clickFecharModal(){
+        loginMap.divFecharModal.click();
+    }
+    public void setInpUsername(String username){
+        loginMap.inpUserName.sendKeys(username);
+    }
+    public void setInpPassword(String password){
+        loginMap.inpPassword.sendKeys(password);
+    }
+    public void clickInpRemember(){
+        loginMap.inpRemember.click();
+    }
+    public void clickLinkCreateAccount(){
+        loginMap.linkCreateAccount.click();
+    }
+    public  void clickBtnSignIn(){
+        loginMap.btnSignIn.click();
+    }
+    public boolean isBtnSignIn(){
+        return loginMap.btnSignIn.isEnabled();
+    }
 }
